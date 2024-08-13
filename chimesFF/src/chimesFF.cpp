ccb78e62f0dfb56eead76d9e7f2f13fc7a0db923
R1j1t <22280243+R1j1t@users.noreply.github.com>
2024-08-13 15:40:00 -0700

    removed energy computation for now

--- chimesFF/src/chimesFF.cpp
+++ chimesFF/src/chimesFF.cpp
@@ -1858,7 +1858,7 @@ void chimesFF::compute_4B(const vector<double> & dx, const vector<double> & dr, 
         double Tn_jk_jl    =  Tn_jk[ powers[coeffs][3] ] * Tn_jl[ powers[coeffs][4] ] ;
         double Tn_kl_5     =  Tn_kl[ powers[coeffs][5] ] ;
 
-        energy += coeff * fcut_all * Tn_ij_ik_il * Tn_jk_jl * Tn_kl_5 ;        
+        // energy += coeff * fcut_all * Tn_ij_ik_il * Tn_jk_jl * Tn_kl_5 ;        
 
         deriv[0] = fcut[0] * Tnd_ij[ powers[coeffs][0] ] + fcutderiv[0] * Tn_ij[ powers[coeffs][0] ];
         deriv[1] = fcut[1] * Tnd_ik[ powers[coeffs][1] ] + fcutderiv[1] * Tn_ik[ powers[coeffs][1] ];
@@ -1875,7 +1875,7 @@ void chimesFF::compute_4B(const vector<double> & dx, const vector<double> & dr, 
         force_scalar[5]  = coeff * deriv[5] * fcut_5[5] * Tn_ij_ik_il * Tn_jk_jl ;
 
     }
-    
+
     // Accumulate forces/stresses on/from the ij pair
     for(int coeffs=0; coeffs<variablecoeff; coeffs++)
     {
