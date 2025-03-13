eb669025a5017ff8aeb202912266ebf19ea11ae5
Benjamin Robert Laubach <laubach2@ruby964.llnl.gov>
2025-03-13 14:13:36 -0700

    Cleaned directory

--- chimesFF/examples/cpp/lmp_test/run_lmpmd.cmd
+++ chimesFF/examples/cpp/lmp_test/run_lmpmd.cmd
@@ -9,6 +9,3 @@
 #SBATCH -o stdoutmsg
 module load cmake intel impi
 srun -N 1 -n 56 /g/g14/sundberg3/codes/FP_Calc/etc/lmp/exe/lmp_mpi_chimes -i case-0.indep-0.in.lammps  > out.lammps
-for *.*.2b_clusters.txt
-done
-cat 0.*2b_clusters.txt > 
