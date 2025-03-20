3aa3b12fb14a28af9a614e0dbffc635840b8bbf6
Benjamin Robert Laubach <laubach2@ruby962.llnl.gov>
2025-03-20 12:49:08 -0700

    Added parallel file analysis in histogram executable

--- chimesFF/examples/cpp/lmp_test/get_hist.cmd
+++ chimesFF/examples/cpp/lmp_test/get_hist.cmd
@@ -8,5 +8,6 @@
 #SBATCH -V 
 #SBATCH -o stdoutmsg
 
-mpiicc -O3 -o histogram calc_histograms.cpp chimesFF.cpp
-srun -N 1 -n 56 ./histogram 0 0
+mpiicc -O3 -o histogram multi_calc_histogram.cpp chimesFF.cpp
+srun -N 1 -n 56 ./histogram
+rm *.core
