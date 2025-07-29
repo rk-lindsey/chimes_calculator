0ad68976ec017a59b2a8bbc32193535537b1cb9f
Rebecca K. Lindsey <RKLindsey@users.noreply.github.com>
2025-07-29 11:04:54 -0400

    Hotfix: Allow for hybrid overlay pair styles
    Allow for hybrid/overlay pair style. For example, combining ChIMES description for carbon with LJ description of Ar for simulations of carbon nanoparticles in an Ar bath.

--- etc/lmp/src/pair_chimes.cpp
+++ etc/lmp/src/pair_chimes.cpp
@@ -833,13 +833,19 @@ void PairCHIMES::set_chimes_type()
 			}
 		}	}
 
-	
 	if (nmatches < atom->ntypes )
 	{
-		std::cout << "ERROR: LAMMPS coordinate file has " << atom->ntypes << " atom type masses" << std::endl; 
+		// Commented out to allow for hybrid/overlay pair style. For example, combining ChIMES description for carbon 
+		// with LJ description of Ar for simulations of carbon nanoparticles in an argon bath
+		//std::cout << "ERROR: LAMMPS coordinate file has " << atom->ntypes << " atom type masses" << std::endl; 
+		//std::cout << "       but only found " << nmatches << " matches with the ChIMES parameter file." << std::endl;
+		//exit(0);
+ 
+	    std::cout << "WARNING: LAMMPS coordinate file has " << atom->ntypes << " atom type masses" << std::endl;
 		std::cout << "       but only found " << nmatches << " matches with the ChIMES parameter file." << std::endl;
-		exit(0);
-	} 
+		std::cout << "       Will not use ChIMES to evaluate interactions between these pairs! " << std::endl;
+	}
+}
 }
 							
 void PairCHIMES::write_restart(){}			
